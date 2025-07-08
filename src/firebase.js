7d96e6928c8a18d7df54a5ed3e6c88ebb27afbe0
ClarityoverComfort <bolivar.campusano@gmail.com>
2025-07-08 09:26:45 -0400

    Updated firebase configuration in .env file

--- src/firebase.js
+++ src/firebase.js
@@ -1,26 +1,19 @@
-
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
-import { getAnalytics } from "firebase/analytics";
 
-// Firebase configuration for inner-ops project
 const firebaseConfig = {
-  apiKey: "AIzaSyCNO79Mw06MClVux9Fg1qC2-n1sj8mcyqI",
-  authDomain: "inner-ops.firebaseapp.com",
-  projectId: "inner-ops",
-  storageBucket: "inner-ops.firebasestorage.app",
-  messagingSenderId: "196049580582",
-  appId: "1:196049580582:web:fcd232716e233951946ce7",
-  measurementId: "G-K2L34NYKW5"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
-const analytics = getAnalytics(app);
 
-export { auth, db, analytics };
+export { auth, db };
 export default app;
